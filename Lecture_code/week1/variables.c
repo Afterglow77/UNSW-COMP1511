bcb9b46d5817817557720ffa0e95ee032e0ca0c6
Afterglow77 <yang.nova@outlook.com>
2026-01-30 18:41:34 +0800

    variables

---
+++ Lecture_code/week1/variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("My variables program\n");
+
+    int my_age =20;
+
+    int drinking_age;
+
+    drinking_age = 18;
+
+    drinking_age = 21;
+
+    printf("My age is %d", my_age);
+    return 0;
+}
