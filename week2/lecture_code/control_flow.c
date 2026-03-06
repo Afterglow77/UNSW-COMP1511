a03afeaf31e758a6f6a7f7bf81c0e9ded3f5e70e
Afterglow77 <yang.nova@outlook.com>
2026-03-06 12:45:35 +0800

    week2 learning

---
+++ week2/lecture_code/control_flow.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void){
+
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    if(age < 18){
+        printf("You are too young\n");
+    }
+
+    return 0;
+}
