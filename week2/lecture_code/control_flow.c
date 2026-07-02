efdae3a2a800d949f4f929bf55231701ba61fcca
Afterglow77 <yang.nova@outlook.com>
2026-07-02 14:01:12 +0800

    update

--- week2/lecture_code/control_flow.c
+++ week2/lecture_code/control_flow.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
 #define MAIN_AGE 18
-int main(void)
-{
+int main(void) {
 
     int age;
     printf("Enter your age: ");
     scanf("%d", &age);
 
-    if (age <= 0)
-    {
+    if (age <= 0) {
         printf("That is invalid!\n");
     }
     else{
 
-        if (age < MAIN_AGE){
+        if (age < MAIN_AGE) {
         printf("You are too young\n");
-        }else{
+        } else {
             printf("OK, valid!\n");
         }
 
